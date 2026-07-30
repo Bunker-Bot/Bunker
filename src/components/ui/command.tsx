29ebaa792f7ac3d64e707a2c56d9802e6d154441
Bunker Developer <dev@bunker.app>
2026-07-30 16:56:12 +0530

    Fix CommandDialog cmdk context provider in command.tsx

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -54,12 +54,14 @@ function CommandDialog({
       </DialogHeader>
       <DialogContent
         className={cn(
-          "top-1/3 translate-y-0 overflow-hidden rounded-xl! p-0",
+          "top-1/3 translate-y-0 overflow-hidden rounded-xl! p-0 bg-transparent border-none shadow-none",
           className
         )}
         showCloseButton={showCloseButton}
       >
-        {children}
+        <Command className="[&_[data-slot=command-input-wrapper]]:h-12 [&_[data-slot=command-input-wrapper]_svg]:size-5 [&_[data-slot=command-input-wrapper]_input]:h-12 [&_[data-slot=command-item]]:px-2 [&_[data-slot=command-item]]:py-3 [&_[data-slot=command-item]_svg]:size-5">
+          {children}
+        </Command>
       </DialogContent>
     </Dialog>
   )
